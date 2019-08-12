889701f05bc2f60a9dc5c4f2e38d5f6878207f6b
The Sharp Ninja <ninja@thesharp.ninja>
2019-08-12 09:58:09 -0500

    Reorganized to reflect standard GIT structure for .net Projects.
    Signed-off-by: The Sharp Ninja <ninja@thesharp.ninja>

---
+++ src/Deploy.ps1
@@ -0,0 +1,41 @@
+function Push-Package
+{
+    param(
+        [Parameter(Mandatory=$true)][string]$SolutionDirectory,
+        [Parameter(Mandatory=$false)][string]$Config = "Debug",
+        [Parameter(Mandatory=$false)][string]$Repository = "http://localhost:81/nuget/GPS",
+        [Parameter(Mandatory=$false)][string]$ApiKey = $env:LocalhostNugetAPIKey
+    )
+
+    #$SolutionName = (Get-ChildItem $SolutionDirectory"/*.sln" | Get-Item).Name
+
+    $OutputPath = [System.IO.Path]::Combine($SolutionDirectory, "Outbound")
+    $ProjectPath = [System.IO.Path]::Combine($SolutionDirectory, "GPS.SimpleHelpers", "GPS.SimpleHelpers.csproj")
+    $NuSpecPath = [System.IO.Path]::Combine($SolutionDirectory, "GPS.SimpleHelpers", "GPS.SimpleHelpers.nuspec")
+
+    Write-Host $OutputPath
+
+    Get-Item $OutputPath -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse
+    New-Item -Path $SolutionDirectory -Name "Outbound" -ItemType "directory"
+
+    # dotnet build $SolutionDirectory -c $Config
+
+    Write-Host $ProjectPath
+
+    nuget pack $NuSpecPath -OutputDirectory $OutputPath -Properties Configuration=$Config
+
+	if($LASTEXITCODE -eq 0) {
+        $Packages = Get-Item $OutputPath -ErrorAction Stop | Get-ChildItem
+
+        $Packages | ForEach-Object -Process {
+            $Command = "dotnet nuget push " + $_.FullName + " -s " + $Repository + " -k " + $ApiKey
+            # Write-Host $Command
+            Invoke-Expression $Command
+        }
+	}
+}
+
+Clear-Host
+
+#Deploy-Package -SolutionDir %1 -BuildDir %2 -Namespace %3 -Assembly %4
+Push-Package -SolutionDir 'd:\GitHub\SimpleHelpers\GPS.SimpleHelpers' -Repository "http://source.marcumllp.com:81/nuget/Default" -ApiKey "pbyrd:pbyrd"
